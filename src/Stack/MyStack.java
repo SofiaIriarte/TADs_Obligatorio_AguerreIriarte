adf215d78b935adbc20ff8bd979e53e7aa2454da
SIriarte <siriarte@correo.um.edu.uy>
2020-05-29 23:48:48 -0300

    add

--- src/Stack/MyStack.java
+++
@@ -1,9 +0,0 @@
-package Stack;
-
-public interface MyStack <T>{
-    void pop() throws EmptyStackException ;
-    T top() throws EmptyStackException ;
-    void push(Nodo<T> element) throws EmptyStackException ;
-    boolean isEmpty();
-    void makeEmpty() throws EmptyStackException;
-}
