125fe27cf99b32b4b0218d1fa1a334b397ac4b53
SIriarte <siriarte@correo.um.edu.uy>
2020-05-29 22:14:22 -0300

    add

---
+++ src/CircularLinkedList/CircularLinkedList.java
@@ -0,0 +1,72 @@
+package CircularLinkedList;
+
+import LinkedList.Nodo;
+
+public class CircularLinkedList<T> {
+    private NodoDouble<T> primero;
+    private NodoDouble<T> siguiente = null;
+    private NodoDouble<T> anterior = null;
+    private NodoDouble<T> end;
+
+    public CircularLinkedList() {
+    }
+
+    public void add(T valor) throws ExceptionListaVacia {
+        NodoDouble<T> agregar = new NodoDouble(valor);
+        if (this.primero.equals((Object)null)) {
+            throw new ExceptionListaVacia();
+        } else {
+            NodoDouble nodoSiguiente;
+            for(nodoSiguiente = this.primero.getSiguiente(); nodoSiguiente.getSiguiente() != null && nodoSiguiente != this.primero; nodoSiguiente = nodoSiguiente.getSiguiente()) {
+            }
+
+            if (nodoSiguiente.equals(this.primero)) {
+                NodoDouble<T> ultimo = this.end;
+                ultimo = this.primero.getAnterior();
+                ultimo.setSiguiente(agregar);
+                agregar.setAnterior(ultimo);
+                agregar.setSiguiente(this.primero);
+                this.primero.setAnterior(agregar);
+            }
+
+        }
+    }
+
+    public void remove(int position) {
+        NodoDouble<T> actual = this.primero;
+        NodoDouble<T> before = null;
+        int count = 0;
+
+        while(position != count) {
+            if (actual.equals(this.primero)) {
+                count = 0;
+            }
+
+            if (actual.equals((Object)null)) {
+                System.out.println("La lista esta vacia");
+                break;
+            }
+
+            actual = actual.getSiguiente();
+            ++count;
+            if (position == count && actual != null) {
+                actual.setSiguiente((NodoDouble)null);
+                actual.setAnterior((NodoDouble)null);
+                before = actual.getAnterior();
+                before.setSiguiente(actual.getSiguiente());
+            }
+        }
+
+    }
+
+    public T get(int position){
+        NodoDouble<T> buscado = this.primero;
+        T value = null;
+        for (int i=0; i < position; i++) {
+            buscado = buscado.getSiguiente();
+        }
+        value = buscado.getValor();
+        return value;
+    }
+
+}
